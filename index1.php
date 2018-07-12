c78eea8c058b7350762f240195d4c5067a97110c
Avneet <virk@gmail.com>
2018-07-12 22:45:56 +0530

    again chaged the title

--- index1.php
+++ index1.php
@@ -15,7 +15,7 @@ include 'includes/head.php'; ?>
 	<div class="row no-gutters">
 			<div class="col-md-1 brand-logo"><a href="index.php"><img src="images/medical-logo.jpg" class="brand-logo"></a></div>
 			<div class="col-md-3 brand-title">
-				<h2><a href="index.php">hosptial manaGEMENT SYSTEm</a></h2>
+				<h2><a href="index.php">Hosptial management system</a></h2>
 			</div>
 
 	<div class="col-md-8">
